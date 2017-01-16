fcae6b27141d70ced17d0fc678254de6ac923293
Swift <swift@WozMac8.local>
2017-01-16 20:03:23 -0200

    Initial Commit

---
+++ Aula_001_VisualizadorDeImagens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aula_001_VisualizadorDeImagens
+//
+//  Created by Swift on 16/01/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
