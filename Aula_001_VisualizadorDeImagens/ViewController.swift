09b2cd7415b39e84a2ecdc5cad478ab0c919b849
Swift <swift@WozMac8.local>
2017-01-16 22:07:50 -0200

    inicio

--- Aula_001_VisualizadorDeImagens/ViewController.swift
+++ Aula_001_VisualizadorDeImagens/ViewController.swift
@@ -7,18 +7,68 @@
 //
 
 import UIKit
+import QuickLook
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, QLPreviewControllerDataSource, QLPreviewControllerDelegate{
+    
+    //MARK: Outlets
+    @IBOutlet weak var imagem: UIImageView!
+    
+    //MARK: Propriedades
+    var arrayNomesFotos : [String] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 1...4 {
+            let nomeFoto = "IMG_\(i)"
+            self.arrayNomesFotos += [nomeFoto]
+        }
+
+    }
+    
+    //MARK: Actions
+    @IBAction func escolher(_ sender: UIButton) {
+        
+        let quickLook = QLPreviewController()
+        quickLook.delegate = self
+        quickLook.dataSource = self
+        
+        quickLook.refreshCurrentPreviewItem()
+        
+        quickLook.currentPreviewItemIndex = 0
+        
+        self.present(quickLook, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: Métodos de QLPreviewControllerDataSource
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return arrayNomesFotos.count
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        
+        let nome = arrayNomesFotos[index]
+        let ext = "JPG"
+        let raizApp = Bundle.main
+        let pathArquivo = raizApp.path(forResource: nome, ofType: ext)
+        let urlArquivo = URL(fileURLWithPath: pathArquivo!)
+        return urlArquivo as QLPreviewItem
+        
+        
+    }
+    
+    
+    //MARK: Métodos de QLPreviewControllerDelegate
+    func previewControllerWillDismiss(_ controller: QLPreviewController) {
+        let indice = controller.currentPreviewItemIndex
+        let nomeCompleto = "\(arrayNomesFotos[indice]).JPG"
+        imagem.image = UIImage(named: nomeCompleto)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
